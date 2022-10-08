83cd8be26d266de3c8a540887f99fcbf6a8fdc2c
xdgonex <bendezucortezjoseluis@gmail.com>
2022-10-07 19:45:47 -0500

    Se creeo la base indexed en el archivo app.js y sus respectivas columnas

---
+++ js/app.js
@@ -0,0 +1,37 @@
+(function(){    //IFI
+
+    let DB;
+
+    document.addEventListener('DOMContentLoaded',()=>{
+        crearBD();
+    })
+
+    function crearBD(){
+
+        const crearBD = window.indexedDB.open('crm',1);
+
+        crearBD.onerror = function(){
+            console.log('No se pudo crear la BDindex');
+        }
+
+        crearBD.onsuccess = function(){
+            DB = crearBD.result;
+            console.log('Se creo la BDindex');
+        }
+
+        crearBD.onupgradeneeded = function(e){
+            const db = e.target.result;
+
+            const objectStore = db.createObjectStore('crm',{
+                KeyPath: 'id',
+                autoIncrement: true
+            });
+
+            objectStore.createIndex('nombre','nombre',{unique:false});
+            objectStore.createIndex('email','email',{unique:false});
+            objectStore.createIndex('telefono','telefono',{unique:false});
+            objectStore.createIndex('empresa','empresa',{unique:false});
+            objectStore.createIndex('id','id',{unique:true});
+        }
+    }
+})();
