af312a6138d355a280514b65039a5dfebe9f0ea3
xdgonex <bendezucortezjoseluis@gmail.com>
2022-10-09 02:47:50 -0500

    Se realizo la funcion eliminar, y extraer el elemento del HTML, actualizacion de la indexedDB

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 (function(){    //IFI
 
     let DB;
+    const lista_clientes = document.querySelector('#listado-clientes');
 
     document.addEventListener('DOMContentLoaded',()=>{
         crearBD();
@@ -9,6 +10,8 @@
         {
             obtenerClientes();
         }
+
+        lista_clientes.addEventListener('click',eliminarRegistro);
     })
 
     function crearBD(){
@@ -40,6 +43,30 @@
         }
     }
 
+    function eliminarRegistro(e)
+    {
+        if(e.target.classList.contains('eliminar')){
+            const idEliminar = Number(e.target.dataset.cliente);
+
+            const confirmar = confirm('Deseas Eliminar este cliente');
+
+            if(confirmar)
+            {
+                const transaccion = DB.transaction(['crm'],'readwrite');
+                const objectStore = transaccion.objectStore('crm');
+
+                objectStore.delete(idEliminar);
+
+                transaccion.oncomplete = function(){
+                    e.target.parentElement.parentElement.remove();
+                }
+
+                transaccion.onerror = function(){
+                    console.log('No se pudo eliminar');
+                }
+            }
+        }
+    }
     function obtenerClientes()
     {
         const abrirConexion = window.indexedDB.open('crm',1);
@@ -59,8 +86,6 @@
                 if(cursor)
                 {
                     const {nombre,email,telefono,empresa,id} = cursor.value;
-
-                    const lista_clientes = document.querySelector('#listado-clientes');
                     
                     lista_clientes.innerHTML += `
                     <tr>
@@ -76,7 +101,7 @@
                         </td>
                         <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                             <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                            <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                            <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
                         </td>
                     </tr>
                     `;
